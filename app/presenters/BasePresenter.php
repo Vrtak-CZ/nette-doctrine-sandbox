213dec29e5f1459397d4823b4e3fc614d84849c2
Patrik Votocek <patrik@votocek.cz>
2012-09-25 05:25:41 +0200

    use autowire

--- app/presenters/BasePresenter.php
+++ app/presenters/BasePresenter.php
@@ -8,5 +8,15 @@
  */
 abstract class BasePresenter extends Nette\Application\UI\Presenter
 {
+	/** @var Doctrine\ORM\EntityManager */
+	protected $em;
 
+	public function injectEntityManager(\Doctrine\ORM\EntityManager $em)
+	{
+		if ($this->em) {
+			throw new \Nette\InvalidStateException('Entity manager has already been set');
+		}
+		$this->em = $em;
+		return $this;
+	}
 }
--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -10,24 +10,22 @@ class HomepagePresenter extends BasePresenter
 {
 	public function actionCreateDefaultUser()
 	{
-		$em = $this->getContext()->database;
-		
 		$user = new User('admin');
 		$user->setPassword($this->getContext()->authenticator->calculateHash('traktor'));
 		$user->setEmail('info@nella-project.org')->setRole('admin');
-		
-		$em->persist($user);
+
+		$this->em->persist($user);
 		try {
-			$em->flush();
+			$this->em->flush();
 		} catch(\PDOException $e) {
 			dump($e);
 			$this->terminate();
 		}
-		
+
 		$this->sendResponse(new \Nette\Application\Responses\TextResponse('OK'));
 		$this->terminate();
 	}
-	
+
 
 	public function renderDefault()
 	{
